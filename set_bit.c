10934e4c389da8394d443b20b89b8e4c0dbcaba6
Replit Me <49104171+carsonak@users.noreply.github.com>
2023-11-01 10:59:37 +0000

    Added spacing

--- set_bit.c
+++ set_bit.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 unsigned int set_bit(unsigned int num, unsigned int bit);
+void print_binary(unsigned int num, unsigned int spc);
 
 unsigned int set_bit(unsigned int num, unsigned int index)
 {
@@ -12,26 +13,49 @@ unsigned int set_bit(unsigned int num, unsigned int index)
 	return (num);
 }
 
-void print_binary(unsigned int num)
+void print_binary(unsigned int num, unsigned int spc)
 {
-	while (num)
-	{
-		printf("%d", num & 1);
-		num >>= 1;
-	}
+	if (num == 0)
+		return;
+
+	spc++;
+	print_binary(num >> 1, spc++);
+	
+	printf("%d", num & 1);
+	if (!(spc % 4) && spc > 4)
+		putchar(' ');
+	
 }
 
 int main(void)
 {
-	unsigned int num = 3421, num2 = 0, idx = 40;
+	unsigned int num = 341, num2 = 0, idx = 40;
 	
 	printf("Before = %u [", num);
-	print_binary(num);
+	print_binary(num, 1);
 	printf("]\n");
 	num2 = set_bit(num, idx);
-	printf("After = %u [", num2);
-	print_binary(num2);
+	printf("%u set at %u = %u [", num, idx, num2);
+	print_binary(num2, 1);
+	printf("]\n\n");
+
+	idx = 13;
+	printf("Before = %u [", num);
+	print_binary(num, 1);
 	printf("]\n");
+	num2 = set_bit(num, idx);
+	printf("%u set at %u = %u [", num, idx, num2);
+	print_binary(num2, 1);
+	printf("]\n\n");
+
+	idx = -1;
+	printf("Before = %u [", num);
+	print_binary(num, 1);
+	printf("]\n");
+	num2 = set_bit(num, idx);
+	printf("%u set at %u = %u [", num, idx, num2);
+	print_binary(num2, 1);
+	printf("]\n\n");
 
 	return (0);
 }
