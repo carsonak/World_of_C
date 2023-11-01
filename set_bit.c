ae083516a61aa73352e459714b551b01babbfab3
Replit Me <49104171+carsonak@users.noreply.github.com>
2023-11-01 10:34:38 +0000

    added print binary functinality

--- set_bit.c
+++ set_bit.c
@@ -12,13 +12,26 @@ unsigned int set_bit(unsigned int num, unsigned int index)
 	return (num);
 }
 
+void print_binary(unsigned int num)
+{
+	while (num)
+	{
+		printf("%d", num & 1);
+		num >>= 1;
+	}
+}
+
 int main(void)
 {
-	unsigned int num = 9, num2 = 0;
+	unsigned int num = 3421, num2 = 0, idx = 40;
 	
-	printf("Before = %u\n", num);
-	num2 = set_bit(num, 5);
-	printf("After = %u\n", num2);
+	printf("Before = %u [", num);
+	print_binary(num);
+	printf("]\n");
+	num2 = set_bit(num, idx);
+	printf("After = %u [", num2);
+	print_binary(num2);
+	printf("]\n");
 
 	return (0);
 }
