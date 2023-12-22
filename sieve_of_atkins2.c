b106fc014efb15c8f6faa2ff3b7ba076a067b840
Andrew <49104171+carsonak@users.noreply.github.com>
2023-12-22 16:26:20 +0300

    Bit mask sieve of Atkin's

--- sieve_of_atkins2.c
+++ sieve_of_atkins2.c
@@ -18,7 +18,7 @@ int only_digits(char *str);
 int main(int argc, char *argv[])
 {
 
-	int64_t limit = 0, wlimit = 0;
+	int64_t limit = 0, ulimit = 0;
 	int64_t i, j, count, x, y, z;
 	uint8_t *sieb = NULL;
 
@@ -35,48 +35,63 @@ int main(int argc, char *argv[])
 	}
 
 	limit = strtol(argv[1], NULL, 10);
-	sieb = calloc(limit, sizeof(*sieb));
+	sieb = calloc(((limit / 8) + 1), sizeof(*sieb));
 	if (!sieb)
 	{
 		fprintf(stderr, "A memory error occured\n");
 		return (EXIT_FAILURE);
 	}
 
-	wlimit = sqrt(limit);
-	for (x = 1; x <= wlimit; x++)
+	ulimit = sqrt(limit);
+	for (x = 1; x <= ulimit; x++)
 	{
-		for (y = 1; y <= wlimit; y++)
+		for (y = 1; y <= ulimit; y++)
 		{
 			z = (4 * x * x) + (y * y);
 			if (z <= limit &&
 				(z % 60 == 1 || z % 60 == 13 || z % 60 == 17 || z % 60 == 29 ||
 				 z % 60 == 37 || z % 60 == 41 || z % 60 == 49 || z % 60 == 53))
-				sieb[z] = !sieb[z];
+			{
+				if (sieb[z / 8] & (1 << (z % 8)))
+					sieb[z / 8] &= ~(1 << (z % 8));
+				else
+					sieb[z / 8] |= (1 << (z % 8));
+			}
 
 			z = (3 * x * x) + (y * y);
 			if (z <= limit &&
 				(z % 60 == 7 || z % 60 == 19 || z % 60 == 31 || z % 60 == 43))
-				sieb[z] = !sieb[z];
+			{
+				if (sieb[z / 8] & (1 << (z % 8)))
+					sieb[z / 8] &= ~(1 << (z % 8));
+				else
+					sieb[z / 8] |= (1 << (z % 8));
+			}
 
 			z = (3 * x * x) - (y * y);
 			if (x > y && z <= limit &&
 				(z % 60 == 11 || z % 60 == 23 || z % 60 == 47 || z % 60 == 59))
-				sieb[z] = !sieb[z];
+			{
+				if (sieb[z / 8] & (1 << (z % 8)))
+					sieb[z / 8] &= ~(1 << (z % 8));
+				else
+					sieb[z / 8] |= (1 << (z % 8));
+			}
 		}
 	}
 
-	for (i = 5; i <= wlimit; i++)
-		if (sieb[i])
+	for (i = 5; i <= ulimit; i++)
+		if (sieb[i / 8] & (1 << (i % 8)))
 			for (j = 1; (j * i * i) <= limit; j++)
-				sieb[j * i * i] = 0;
+				sieb[(j * i * i) / 8] &= ~(1 << ((j * i * i) % 8));
 
-	sieb[2] = 1;
-	sieb[3] = 1;
-	sieb[5] = 1;
+	sieb[2 / 8] |= (1 << (2 % 8));
+	sieb[3 / 8] |= (1 << (3 % 8));
+	sieb[5 / 8] |= (1 << (5 % 8));
 	printf("Primes in range %ld:\n", limit);
 	for (i = 0, count = 0; i <= limit; i++)
 	{
-		if (sieb[i])
+		if (sieb[i / 8] & (1 << (i % 8)))
 		{
 			printf("%5ld ", i);
 			if (!(++count % 16))
